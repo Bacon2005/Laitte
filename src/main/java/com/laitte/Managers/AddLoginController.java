00f6a2b2833576493a110cbbdebb84bec8150d98
TYRONE-PC\User <niere.tyrone@gmail.com>
2025-12-03 15:58:51 +0800

    Add Login Controller

--- src/main/java/com/laitte/Managers/AddLoginController.java
+++ src/main/java/com/laitte/Managers/AddLoginController.java
@@ -1,9 +1,5 @@
 package com.laitte.Managers;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -39,13 +35,13 @@ public class AddLoginController {
     @FXML
     private CheckBox isPasswordVisible;
 
-    //===============================================================================//
-    public void initialize(){
+    // ===============================================================================//
+    public void initialize() {
         passwordFieldFirstVisible.textProperty().bindBidirectional(passwordFieldFirst.textProperty());
         passwordFieldConfirmVisible.textProperty().bindBidirectional(passwordFieldConfirm.textProperty());
     }
 
-     @FXML
+    @FXML
     private void cancelBtn(ActionEvent event) {
         // Close the add employee window
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -54,62 +50,63 @@ public class AddLoginController {
 
     // @FXML
     // private void confirmBtn(ActionEvent event) {
-    //     String username = usernameField.getText();
-    //     String password = passwordFieldFirst.getText();
-    //     String confirm = passwordFieldConfirm.getText();
-    //     if(password != confirm){
-    //         System.out.println("Error not the same password");
-    //     } 
+    // String username = usernameField.getText();
+    // String password = passwordFieldFirst.getText();
+    // String confirm = passwordFieldConfirm.getText();
+    // if(password != confirm){
+    // System.out.println("Error not the same password");
+    // }
 
-    //     String insertRole = """
-    //                 INSERT INTO employeerole (rolename, ismanager)
-    //                 VALUES (?, ?)
-    //                 RETURNING roleid
-    //             """;
+    // String insertRole = """
+    // INSERT INTO employeerole (rolename, ismanager)
+    // VALUES (?, ?)
+    // RETURNING roleid
+    // """;
 
-    //     String insertEmployee = """
-    //                 INSERT INTO employee (firstname, lastname, roleid, imagepath)
-    //                 VALUES (?, ?, ?, ?)
-    //             """;
+    // String insertEmployee = """
+    // INSERT INTO employee (firstname, lastname, roleid, imagepath)
+    // VALUES (?, ?, ?, ?)
+    // """;
 
-    //     try (Connection conn = com.laitte.LaitteMain.Database.connect()) {
-    //         conn.setAutoCommit(false);
+    // try (Connection conn = com.laitte.LaitteMain.Database.connect()) {
+    // conn.setAutoCommit(false);
 
-    //         // INSERT ROLE AND GET roleid
-    //         int roleID;
-    //         try (PreparedStatement pstmtRole = conn.prepareStatement(insertRole)) {
-    //             pstmtRole.setString(1, role);
-    //             pstmtRole.setBoolean(2, managerStatus);
+    // // INSERT ROLE AND GET roleid
+    // int roleID;
+    // try (PreparedStatement pstmtRole = conn.prepareStatement(insertRole)) {
+    // pstmtRole.setString(1, role);
+    // pstmtRole.setBoolean(2, managerStatus);
 
-    //             ResultSet rs = pstmtRole.executeQuery();
-    //             rs.next();
-    //             roleID = rs.getInt("roleid");
-    //         }
+    // ResultSet rs = pstmtRole.executeQuery();
+    // rs.next();
+    // roleID = rs.getInt("roleid");
+    // }
 
-    //         // INSERT EMPLOYEE WITH roleid
-    //         try (PreparedStatement pstmtEmployee = conn.prepareStatement(insertEmployee)) {
-    //             pstmtEmployee.setString(1, firstName);
-    //             pstmtEmployee.setString(2, lastName);
-    //             pstmtEmployee.setInt(3, roleID);
-    //             if (profilePicPath.isEmpty()) {
-    //                 pstmtEmployee.setString(4, null);
-    //             } else {
-    //                 pstmtEmployee.setString(4, profilePicPath);
-    //             }
+    // // INSERT EMPLOYEE WITH roleid
+    // try (PreparedStatement pstmtEmployee = conn.prepareStatement(insertEmployee))
+    // {
+    // pstmtEmployee.setString(1, firstName);
+    // pstmtEmployee.setString(2, lastName);
+    // pstmtEmployee.setInt(3, roleID);
+    // if (profilePicPath.isEmpty()) {
+    // pstmtEmployee.setString(4, null);
+    // } else {
+    // pstmtEmployee.setString(4, profilePicPath);
+    // }
 
-    //             pstmtEmployee.executeUpdate();
-    //         }
+    // pstmtEmployee.executeUpdate();
+    // }
 
-    //         conn.commit();
-    //         System.out.println("Employee + Role inserted successfully!");
+    // conn.commit();
+    // System.out.println("Employee + Role inserted successfully!");
 
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
 
-    //     // Close the window after confirming
-    //     Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    //     stage.close();
+    // // Close the window after confirming
+    // Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    // stage.close();
     // }
 
     @FXML
@@ -133,4 +130,3 @@ public class AddLoginController {
         }
     }
 }
-
